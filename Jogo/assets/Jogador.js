7481188c5a9235ecddcd77ad54aa40dbcf34d250
LAPTOP-J9LFQA3L\Bugan <dones.bugan@gmail.com>
2017-11-07 14:15:50 -0200

    Video 1.4

--- Jogo/assets/Jogador.js
+++ Jogo/assets/Jogador.js
@@ -8,7 +8,8 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-      
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.teclaPressionada, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.teclaSolta, this);
     },
 
     // called every frame, uncomment this function to activate update callback
@@ -19,5 +20,44 @@ cc.Class({
         this.node.position = this.node.position.add(deslocamento);
     },
     
-   
+    teclaPressionada : function(event)
+    {
+       
+        if(event.keyCode == cc.KEY.w)
+        {
+            this.direcao.y = 1;
+        }
+         if(event.keyCode == cc.KEY.s)
+        {
+            this.direcao.y = -1;
+        }
+         if(event.keyCode == cc.KEY.a)
+        {
+            this.direcao.x = -1;
+        }
+         if(event.keyCode == cc.KEY.d)
+        {
+            this.direcao.x = 1;
+        }
+    },
+    
+    teclaSolta : function(event)
+    {
+        if(event.keyCode == cc.KEY.w)
+        {
+            this.direcao.y = 0;
+        }
+         if(event.keyCode == cc.KEY.s)
+        {
+            this.direcao.y = 0;
+        }
+         if(event.keyCode == cc.KEY.a)
+        {
+            this.direcao.x = 0;
+        }
+         if(event.keyCode == cc.KEY.d)
+        {
+            this.direcao.x = 0;
+        }
+    },
 });
